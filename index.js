1b5fbd8046fccb73cf98e62b7c511382865c6e5b
KilerBots <davidmersey707@gmail.com>
2025-02-19 11:08:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,142 +1,153 @@
-const axios = require('axios');
-const morgan = require('morgan');
-const express = require('express');
-const PDFDocument = require('pdfkit');
+const axios = require('axios')
+const morgan = require('morgan')
+const express = require('express')
+const PDFDocument = require('pdfkit')
 
-const app = express();
-app.set('json spaces', 4);
-app.use(morgan('dev'));
-app.use(express.json());
+const app = express()
+app.set('json spaces', 4)
+app.use(morgan('dev'))
+app.use(express.json())
 
 function toPDF(images, opt = {}) {
-	return new Promise(async (resolve, reject) => {
-		if (!Array.isArray(images)) images = [images];
-		let buffs = [], doc = new PDFDocument({ margin: 0, size: 'A4' });
-		for (let x = 0; x < images.length; x++) {
-			if (/.webp|.gif/.test(images[x])) continue;
-			let data = (await axios.get(images[x], { responseType: 'arraybuffer', ...opt })).data;
-			doc.image(data, 0, 0, { fit: [595.28, 841.89], align: 'center', valign: 'center' });
-			if (images.length != x + 1) doc.addPage();
-		}
-		doc.on('data', (chunk) => buffs.push(chunk));
-		doc.on('end', () => resolve(Buffer.concat(buffs)));
-		doc.on('error', (err) => reject(err));
-		doc.end();
-	});
+return new Promise(async (resolve, reject) => {
+if (!Array.isArray(images)) images = [images]
+let buffs = [], doc = new PDFDocument({ margin: 0, size: 'A4' })
+for (let x = 0; x < images.length; x++) {
+if (/.webp|.gif/.test(images[x])) continue
+let data = (await axios.get(images[x], { responseType: 'arraybuffer', ...opt })).data
+doc.image(data, 0, 0, { fit: [595.28, 841.89], align: 'center', valign: 'center' })
+if (images.length != x + 1) doc.addPage()
+}
+doc.on('data', (chunk) => buffs.push(chunk))
+doc.on('end', () => resolve(Buffer.concat(buffs)))
+doc.on('error', (err) => reject(err))
+doc.end()
+})
 }
 
-let baseUrl = 'https://cin.guru/';
+async function nhentaiDL(id) {
+const html = await (await axios.get('https://nhentai.net/g/63849'+id)).data
+const match = html.match(/JSON\.parse\((['"`])(.+?)\1\)/)
 
-async function nh(id) {
-	let uri = id ? baseUrl + `v/${+id}/` : baseUrl;
-	let html = (await axios.get(uri)).data;
-	return JSON.parse(html.split('<script id="__NEXT_DATA__" type="application/json">')[1].split('</script>')[0]).props.pageProps.data;
+if(match) {
+let json = match[2].replace(/\\"/g, '"').replace(/\\u([\dA-Fa-f]{4})/g, (m, g) => String.fromCharCode(parseInt(g, 16)))
+let data = JSON.parse(json)
+data.images.pages = data.images.pages.map((v, i) => `https://zorocdn.xyz/galleries/${data.media_id}/${i + 1}.jpg`)
+data.images.cover = `https://zorocdn.xyz/galleries/${data.media_id}/cover.jpg`
+data.images.thumbnail = `https://zorocdn.xyz/galleries/${data.media_id}/thumb.jpg`
+data.tags = data.tags.map(tags => tags.type)
+return data
 }
 
-async function getID(id) {
-	return new Promise(async (resolve, reject) => {
-		try {
-			let data = await nh(id);
-			let pages = data.images.pages.map((v, i) => {
-				let ext = new URL(v.t).pathname.split('.')[1];
-				return `https://zorocdn.xyz/galleries/${data.media_id}/${i + 1}.${ext}`;
-			});
-			let tags = data.tags.reduce((acc, tag) => {
-				acc[tag.type] = acc[tag.type] || [];
-				acc[tag.type].push(tag.name);
-				return acc;
-			}, {});
-			resolve({
-				id: data.id,
-				title: data.title,
-				thumb: `https://zorocdn.xyz/galleries/3069264/thumb.jpg/${data.media_id}/thumb.jpg`,
-				pages,
-				tag: tags.tag || [],
-				artist: tags.artist || [],
-				category: tags.category || [],
-				language: tags.language || [],
-				media_id: data.media_id,
-				num_pages: pages.length,
-				upload_date: data.upload_date
-			});
-		} catch (err) {
-			resolve({ message: err.message });
-		}
-	});
 }
+
+async function nhentaiSearch(query) {
+try {
+const { data } = await axios.get(`https://nhentai.net/search/?q=${query}`)
+const $ = cheerio.load(data)
+const result = []
+
+$('.gallery').each((i, el) => {
+result.push({
+title: $(el).find('.caption').text().trim(),
+thumb: $(el).find('.lazyload').attr("data-src").trim(),
+link: 'https://nhentai.net'+$(el).find('a').attr("href").trim(),
+})
+})
+
+return result
+} catch (e) {
+console.log(e)
+}
+}
+
 
 app.all('/', (req, res) => {
-	const baseUrl = `https://${req.get('host')}`;
-	res.json({
-		runtime: new Date(process.uptime() * 1000).toTimeString().split(' ')[0],
-		endpoint: {
-			detail: `${baseUrl}/detail?code=212121`,
-			read: `${baseUrl}/read?code=212121`,
-			pdf: `${baseUrl}/pdf?code=212121`
-		},
-	});
-});
+const baseUrl = `https://${req.get('host')}`
+res.json({
+runtime: new Date(process.uptime() * 1000).toTimeString().split(' ')[0],
+endpoint: {
+detail: `${baseUrl}/detail?code=212121`,
+search: `${baseUrl}/search?query=yuusha`,
+pdf: `${baseUrl}/pdf?code=212121`,
+read: `${baseUrl}/read?code=212121`,
+},
+})
+})
 
 app.get('/detail', async (req, res) => {
-	const code = req.query.code;
-	if (!code) return res.json({ message: 'Input parameter code' });
-	try {
-		const result = await getID(code);
-		res.json(result);
-	} catch (e) {
-		console.error(e);
-		res.json({ message: e.message });
-	}
-});
+const code = req.query.code
+if (!code) return res.json({ message: 'Input parameter code' })
+try {
+const result = await nhentaiDL(code)
+res.json(result)
+} catch (e) {
+console.error(e)
+res.json({ message: e.message })
+}
+})
+
+app.get('/search', async (req, res) => {
+const { query } = req.query
+if (!query) return res.json({ message: 'Input parameter query' })
+try {
+const result = await nhentaiSearch(query)
+res.json(result)
+} catch (e) {
+console.error(e)
+res.json({ message: e.message })
+}
+})
+
+app.get('/pdf', async (req, res) => {
+const code = req.query.code
+if (!code) return res.json({ message: 'Input parameter code' })
+try {
+const result = await nhentaiDL(code)
+const pdfBuffer = await toPDF(result.pages)
+res.set({
+'Content-Type': 'application/pdf',
+'Content-Disposition': `attachment; filename="${result.title.english || 'document'}.pdf"`
+})
+res.send(pdfBuffer)
+} catch (e) {
+console.error(e)
+res.json({ message: e.message })
+}
+})
+
 
 app.get('/read', async (req, res) => {
-	const code = req.query.code;
-	if (!code) return res.json({ message: 'Input parameter code' });
-	try {
-		const result = await getID(code);
-		const images = await Promise.all(
-			result.pages.map(async (url) => {
-				const response = await axios.get(url, { responseType: 'arraybuffer' });
-				return Buffer.from(response.data).toString('base64');
-			})
-		);
-		const html = `
-			<!DOCTYPE html>
-			<head>
-				<meta charset="UTF-8">
-				<meta name="viewport" content="width=device-width, initial-scale=1.0">
-				<title>${result.title.english}</title>
-				<style>
-					img { display: block; margin: auto; width: 100%; }
-					body { background-color: #1a202c; color: #ffffff; text-align: center; }
-					@media (min-width: 576px) { img { width: auto; max-width: 100%; height: auto; } }
-				</style>
-			</head>
-			<body>
-			    <h3>Server uptime</h3>
-				<h1>${result.title.english}</h1>
-				${images.map((img) => `<img src="data:image/jpeg;base64,${img}">`).join('')}
-			</body>`;
-		res.send(html);
-	} catch (err) {
-		res.json({ error: err.message });
-	}
-});
-app.get('/pdf', async (req, res) => {
-	const code = req.query.code;
-	if (!code) return res.json({ message: 'Input parameter code' });
-	try {
-		const result = await getID(code);
-		const pdfBuffer = await toPDF(result.pages);
-		res.set({
-			'Content-Type': 'application/pdf',
-			'Content-Disposition': `attachment; filename="${result.title.english || 'document'}.pdf"`
-		});
-		res.send(pdfBuffer);
-	} catch (e) {
-		console.error(e);
-		res.json({ message: e.message });
-	}
-});
+const code = req.query.code
+if (!code) return res.json({ message: 'Input parameter code' })
+try {
+const result = await nhentaiDL(code)
+const images = await Promise.all(
+result.pages.map(async (url) => {
+const response = await axios.get(url, { responseType: 'arraybuffer' })
+return Buffer.from(response.data).toString('base64')
+})
+)
+const html = `
+<!DOCTYPE html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>${result.title.english}</title>
+<style>
+img { display: block; margin: auto; width: 100%; }
+body { background-color: #1a202c; color: #ffffff; text-align: center; }
+@media (min-width: 576px) { img { width: auto; max-width: 100%; height: auto; } }
+</style>
+</head>
+<body>
+<h1>${result.title.english}</h1>
+${images.map((img) => `<img src="data:image/jpeg;base64,${img}">`).join('')}
+</body>`
+res.send(html)
+} catch (err) {
+res.json({ error: err.message })
+}
+})
 
-app.listen(3000, () => console.log('App running on port 3000'));
+app.listen(3000, () => console.log('App running on port 3000'))
